04a229d1b961b983a38558bd2f9dee9f0e15ce6f
Bryan Ortiz <bryan.ortiz@ug.uchile.cl>
2023-04-14 15:56:14 -0400

    fix: database insertion on conflict do nothing

--- desc/add_src_desc.sh
+++ desc/add_src_desc.sh
@@ -13,4 +13,4 @@ DB_NAME=$(grep -o '"dbname": "[^"]*' ../db/dbConfig.json | grep -o '[^"]*$')
 export PGPASSWORD=$DB_PASS
 
 psql -h $DB_HOST -U $DB_USER -d $DB_NAME \
-     -c "INSERT INTO external_sources_info (source_id, source_description, source_status) VALUES ('$EXT_SRC_ID', '$EXT_TXT_SRC_DESC', 1);"
+     -c "INSERT INTO external_sources_info (source_id, source_description, source_status) VALUES ('$EXT_SRC_ID', '$EXT_TXT_SRC_DESC', 1) ON CONFLICT DO NOTHING;"
